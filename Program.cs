f458ccf95ccb12a5d9ea61b34b9fa5899b0b1256
Alex <saxenaalex@gmail.com>
2024-11-11 10:43:50 +0100

    Adds user Class & Console Write response

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Write something");
-var userInput = Console.ReadLine();
+Console.Write("What is your name? -> ");
+var userName = Console.ReadLine();
 
-Console.WriteLine("You wrote -> " + userInput);
+Console.Write($"How old are you? -> ");
+
+var userAge = Console.ReadLine();
+
+
+
+
+User newUser = new User
+{
+    Name = userName,
+    Age = userAge
+};
+
+
+Console.WriteLine($"Hello {newUser.Name} you are {newUser.Age}");
+
+
+
+public class User
+{
+    public required string Name { get; set; }
+    public required string Age { get; set; }
+};
+
